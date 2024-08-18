5895d2c59ccc03bddc27e4625f0cbc857eda9fbc
Madan raj <125730836+Madanraj0519@users.noreply.github.com>
2024-08-18 22:55:41 +0530

    Added front-end api

--- Frontend/src/Constant/Backend/constant.js
+++ Frontend/src/Constant/Backend/constant.js
@@ -1,2 +1,2 @@
-// export const BASE_BACKEND_URL = "https://expense-tracker-api-silk.vercel.app";
-export const BASE_BACKEND_URL = "http://localhost:8000";
+export const BASE_BACKEND_URL = "https://expenses-trackers-backend-api.vercel.app";
+// export const BASE_BACKEND_URL = "http://localhost:8000";
